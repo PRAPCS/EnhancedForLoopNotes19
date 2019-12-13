eebdf1d04909804a8fb4240d772f0a7875046098
Mr. Burger <bburger@d155.org>
2019-12-13 16:15:11 +0000

    Original Notes

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
 
     int [] arr = {1,2,3,4,5,6,7,8,9};
     int max = 0;
-
+    //iterates through every slot in the array temporarily setting the value to x.  Cannot change the values in the array.
     for(int x:arr)
     {
       System.out.print(x+"\t");
